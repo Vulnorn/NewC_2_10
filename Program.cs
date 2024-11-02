dea2709545df908dbc0162af42d160bf304a860f
Vulnorn <mixadrbk31@gmail.com>
2024-11-02 15:34:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace NewC_2_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int minNumberRandom = 0;
+            int maxNumberRandom = 100;
+            int specifiedNumber = 2;
+            int degreeNumber = 0;
+            int numberInDegree = 1;
+
+            int randomNumber = random.Next(minNumberRandom, maxNumberRandom);
+
+            for (int i = 0;numberInDegree<randomNumber;i++)
+            {
+                numberInDegree = numberInDegree * specifiedNumber;
+                degreeNumber++;
+            }
+
+            Console.WriteLine($"Число - {randomNumber} минимальная степень числа {specifiedNumber} - {degreeNumber} превосходящее заданное число ({numberInDegree})");
+        }
+    }
+}
